49e17d3b0b15d3b0f30dc20bc7fd149ed6d92271
Nakib00 <2022142@iub.edu.bd>
2024-12-20 00:15:23 +0600

    api optimization

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -1,8 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
+
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Catrgory\CatrgoryController;
 use App\Http\Controllers\Staff\StaffController;
 use App\Http\Controllers\Product\ProductController;
